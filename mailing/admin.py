8da8ce73f3b381eb360e6f61c179d5b1678cf49b
MrchknRV <rodionmarochkin32@gmail.com>
2025-10-17 18:55:37 +0300

    Админка для моделей

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Mailing, MailingAttempt, Message
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("email", "full_name", "owner")
+    list_filter = ("owner",)
+    search_fields = ("email", "full_name")
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("subject", "owner")
+    list_filter = ("owner",)
+    search_fields = ("subject", "body")
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ("id", "start_time", "end_time", "status", "owner")
+    list_filter = ("status", "owner")
+    filter_horizontal = ("clients",)
+
+
+@admin.register(MailingAttempt)
+class MailingAttemptAdmin(admin.ModelAdmin):
+    list_display = ("mailing", "attempt_time", "status")
+    list_filter = ("status", "attempt_time")
+    readonly_fields = ("attempt_time",)
